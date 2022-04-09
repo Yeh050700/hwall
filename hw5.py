b94e707ca89ada853b9f81b4f3861ec523327b2d
Yeh050700 <c110156125@nkust.edu.tw>
2022-04-09 17:59:44 +0800

    Create hw5.py
    hw5 finish

---
+++ hw5.py
@@ -0,0 +1,20 @@
+M = input("輸入乘階值M:")
+n=1
+rag = 1
+tatal=1
+
+while True:
+    rag =rag+1
+    n=n+1
+    tatal =tatal*n  
+    if tatal>M:
+        break
+print("超過M為",M,"的最小階層為",rag)
+
+
+""""
+for i in range(1,8):
+    n =  n*i
+    if n>M: 
+        print("超過M為",M,"的最小階層為",i)
+"""
