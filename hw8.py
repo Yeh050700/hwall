2222d592b0c547a337f308aa55c42c12dd57ee3d
Yeh050700 <c110156125@nkust.edu.tw>
2022-04-10 12:49:28 +0800

    Update hw8.py
    hw8 debug

--- hw8.py
+++ hw8.py
@@ -1,19 +1,16 @@
 from itertools import count
 
 
-t = 8 
+t = int(input("輸入第一行正整數為:"))
 while True :
-    a = input("輸入數字串").split(" ")
-    """print(type(a[0]))"""
-    """a=[4,4,6,2,3,5,8,7]"""
+    a = input("第二行中數列中的數字為:").split(" ")
     if len(a) == t:
         break
-
+    print("格式不符,重新輸入")
 a = list(map(int,a))
 
 amax=(sorted(a,reverse = True))
 s = int(amax[0])
-print(s)
 
 bignum = 0
 tm = 0
@@ -21,11 +18,10 @@ check = 0
 for i in range(0,s):   
     if a.count(i) > bignum :
        tm = i
-       check + 1
        bignum = a.count(i)
-
-if check > 0:
+       check = check + 1
+if check <= 1  :
     print("每個數字剛好出現一次")
 else:
-    print("出現最多次的數字為:",tm,"出現次數:",bignum) 
+    print("最大出現次數的數字為:",tm,"出現次數:",bignum) 
 
