8c449f86f6933227294130d831833ed041c7e8f6
Yeh050700 <c110156125@nkust.edu.tw>
2022-04-10 10:56:08 +0800

    input add
    hw6,hw7, add input

--- hw6.py
+++ hw6.py
@@ -1,4 +1,4 @@
-a=[1,3,5,7,9]
+a= input("輸入值為:").split(",")
 min = sorted(a)
 max = sorted(a,reverse = True)
 
--- hw7.py
+++ hw7.py
@@ -1,32 +1,29 @@
-a =[386,36000]
-
-if a[0] == 186:
-    if a[1] <= 186 :
+a = input("輸入月租費及通話時間為:").split(",")
+if int(a[0]) == 186:
+    if int(a[1]) <= 186 :
         print("通話費為:",a[1]*0.09)
-    elif a[1] < a[0]*2:
-        print("通話費為:",a[1]*0.09*0.9) 
-    elif a[1] > a[0]*2:
-        print("通話費為:",a[1]*0.09*0.8)
-elif a[0] == 386:
-    if a[1] <= 386 :
-        print("通話費為:",a[1]*0.08)
-    elif a[1] < a[0]*2:
-        print("通話費為:",a[1]*0.08*0.8)
-    elif a[1] > a[0]*2:
-        print("通話費為:",a[1]*0.08*0.7)
-elif a[0] == 586:
-    if a[1] <= 586 :
-        print("通話費為:",a[1]*0.07)
-    elif a[1] < a[0]*2:
-        print("通話費為:",a[1]*0.07*0.7)
-    elif a[1] > a[0]*2:
-        print("通話費為:",a[1]*0.07*0.6)
-elif a[0] == 986:
-    if a[1] <= 986 :
-        print("通話費為:",a[1]*0.06)
-    elif a[1] < a[0]*2:
-        print("通話費為:",a[1]*0.06*0.6)
-    elif a[1] > a[0]*2:
-        print("通話費為:",a[1]*0.06*0.5)
-else:
-    print("無此費率")
+    elif int(a[1]) < int(a[0])*2:
+        print("通話費為:",int(a[1])*0.09*0.9) 
+    elif int(a[1]) > int(a[0])*2:
+        print("通話費為:",int(a[1])*0.09*0.8)
+elif int(a[0]) == 386:
+    if int(a[1]) <= 386 :
+        print("通話費為:",int(a[1])*0.08)
+    elif int(a[1]) < int(a[0])*2:
+        print("通話費為:",int(a[1])*0.08*0.8)
+    elif int(a[1]) > int(a[0])*2:
+        print("通話費為:",int(a[1])*0.08*0.7)
+elif int(a[0]) == 586:
+    if int(a[1]) <= 586 :
+        print("通話費為:",int(a[1])*0.07)
+    elif int(a[1]) < int(a[0])*2:
+        print("通話費為:",int(a[1])*0.07*0.7)
+    elif int(a[1]) > int(a[0])*2:
+        print("通話費為:",int(a[1])*0.07*0.6)
+elif int(a[0]) == 986:
+    if int(a[1]) <= 986 :
+        print("通話費為:",int(a[1])*0.06)
+    elif int(a[1]) < int(a[0])*2:
+        print("通話費為:",int(a[1])*0.06*0.6)
+    elif int(a[1]) > int(a[0])*2:
+        print("通話費為:",int(a[1])*0.06*0.5)
