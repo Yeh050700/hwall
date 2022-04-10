c08fcd7711dd1bd6564d2e5070fbc963ee5fddaa
Yeh050700 <c110156125@nkust.edu.tw>
2022-04-10 13:05:14 +0800

    task
    hw10 : 0%   :(

---
+++ Notebook.py
@@ -0,0 +1,14 @@
+"""
+
+
+1.型別轉換    : a = list(map(int,~~~))"     "list str to int"
+2.輸入字串列  : a = input('').split("分隔符號")
+
+3.排串列順序(小到大) :sorted(a) or a.sort
+4.排串列順序(大到小) :sorted(a,reverse = True)
+
+
+
+
+
+"""
